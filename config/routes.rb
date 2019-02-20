fae3c3c20b58b9eacd0dc9bc08c11517eeaae066
Janie Amero <janie.amero@gmail.com>
2019-02-20 13:52:25 +0000

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "restaurants#index"
+  resources :restaurants, only: [:index, :show, :new, :create]
+  resources :restaurants do
+    resources :reviews, only: [:show, :new, :create]
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,47 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    category:  'chinese',
+    phone_number:         '02074209324'
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    category:  'italian',
+    phone_number:         '02077291888'
+  },
+  {
+    name:         'The Five Fields',
+    address:      '8-9 Blacklands Terrace, London SW3 2SP',
+    category:  'french',
+    phone_number:         '02074209324'
+  },
+  {
+    name:         'Ceru',
+    address:      '7-9 Bute St, London SW7 3EY',
+    category:  'belgian',
+    phone_number:         '02031953001'
+  },
+  {
+    name:         'Amaya',
+    address:      'Halkin Arcade, London SW1X 8JT',
+    category:  'chinese',
+    phone_number:         '02074209324'
+  },
+  {
+    name:         'Roka',
+    address:      '37 Charlotte St, London W1T 1RR',
+    category:  'japanese',
+    phone_number:         '02075806464'
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
